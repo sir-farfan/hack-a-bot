be1ad3a3ba07e522974e407e34109fd7aca8d582
Ismael Farfan <sulfur.fff@gmail.com>
2020-06-01 10:46:44 -0500

    Testing Cloud Function

---
+++ multichoice/multi.go
@@ -0,0 +1,25 @@
+package multichoice
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"net/http"
+)
+
+// MultiChoice prints the JSON encoded "message" field in the body
+// of the request or "Hello, World!" if there isn't one.
+func MultiChoice(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Message string `json:"message"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		fmt.Fprint(w, "Hello World!")
+		return
+	}
+	if d.Message == "" {
+		fmt.Fprint(w, "Hello World!")
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(d.Message))
+}
---
+++ multichoice/readme.md
@@ -0,0 +1,1 @@
+This is a cloud function that formats multi-choice options
